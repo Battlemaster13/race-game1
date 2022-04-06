84694b110f84fd4fc8c53dc62077511ea6f3a251
baljeet.hodi <87316125+Battlemaster13@users.noreply.github.com>
2022-04-06 14:01:35 +0300

    Update README.py

--- README.py
+++ README.py
@@ -15,7 +15,7 @@ pygame.display.set_caption("My First Game")
 # www.pngaaa.com
 bk_image = pygame.image.load("background.png")
 car_image = pygame.image.load("car.png")
-car_image = pygame.transform.scale(car_image, (200, 140)) 
+car_image = pygame.transform.scale(car_image, (200, 136)) 
 white = (255, 255, 255)
 velocity = 0 
 
@@ -24,16 +24,15 @@ clock = pygame.time.Clock()
 backgroundx = 0
 backgroundy = 0
 
+# enemy_car_x = random.randint(70,577)
+# enemy_car_y = 0
+
 car_x = WINDOW_W /2 - 102
 car_y = WINDOW_H - 140
 
-x_step = 10
+x_step = 8.2
 
 play = True
-
-# להמשיך אחר כך
-# def car_spawn():
-#   enemy_car_x = random.randint(70,577)
 
 myfont = pygame.font.SysFont('Comic Sans MS', 30)
 textsurface = myfont.render('Score:', True, (255, 255, 255))
@@ -57,18 +56,27 @@ while play:
       if event.key == K_UP:
          velocity = 5
   
+  if keys[pygame.K_UP]:
+    velocity = 5
+  
   backgroundy = backgroundy + velocity
   if backgroundy == 600:
         backgroundy = 0
   
+  # enemy_car_y = enemy_car_y + velocity
+  # if enemy_car_y == 600:
+  #       enemy_car_y = 0
+   
+  
   pygame.draw.line(screen, (255,255,255), (133, 0), (133,650), 4)
 
-
-  screen.blit(car_image,(car_x,car_y))
   textsurface = myfont.render('Score:'+str(score), True, (255, 255, 255))
   screen.blit(textsurface,(10,20))
   screen.blit(bk_image, [backgroundx, backgroundy - 600])
   screen.blit(bk_image, [backgroundx, backgroundy])
+  # screen.blit(bk_image, [backgroundx, backgroundy - 600])
+  # screen.blit(bk_image, [backgroundx, backgroundy])
+  screen.blit(car_image,(car_x,car_y)) 
   
   pygame.display.flip()
 
